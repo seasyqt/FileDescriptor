692dcb545139e0063d738795115e9ff8b4173868
kirilltokarev <kira_man@mail.ru>
2021-04-22 20:28:23 +0300

    Added : FileDescriptorService, app properties, index.html

---
+++ src/main/java/main/service/FileDescriptorService.java
@@ -0,0 +1,55 @@
+package main.service;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import main.model.FileDescriptor;
+import org.json.simple.JSONObject;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileDescriptorService {
+
+  private final static String SAVED_PATH = "files/";
+
+  /**
+   * Сохранение файла в разделе /files
+   * @throws IOException
+   */
+  public static void createdOrUpdatedFile(MultipartFile file, int countSameFiles)
+      throws IOException {
+    StringBuilder pathName;
+    if (countSameFiles != 0) {
+      String nameFile = file.getOriginalFilename();
+      String beforePoint = FileDescriptor.beforePoint(nameFile);
+      String afterPoint = FileDescriptor.afterPoint(nameFile);
+      pathName = new StringBuilder(SAVED_PATH).append(beforePoint).append("(")
+          .append(countSameFiles).append(")")
+          .append(".").append(afterPoint);
+    } else {
+      pathName = new StringBuilder(SAVED_PATH).append(file.getOriginalFilename());
+    }
+    byte[] fileBytes = file.getBytes();
+    BufferedOutputStream stream =
+        new BufferedOutputStream(
+            new FileOutputStream(new File(pathName.toString())));
+    stream.write(fileBytes);
+    stream.close();
+  }
+
+  /**
+   * Информация по файлу из DB в JSON формате
+   * @return JSONObject
+   */
+  public static JSONObject createJSONInfoFileDescriptor(FileDescriptor fileDescriptor) {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("id", fileDescriptor.getId());
+    jsonObject.put("fileName", fileDescriptor.getFileName());
+    jsonObject.put("ext", fileDescriptor.getExt());
+    jsonObject.put("size", fileDescriptor.getSize());
+    jsonObject.put("createdTime", fileDescriptor.getCreatedTs());
+    return jsonObject;
+  }
+
+
+}
